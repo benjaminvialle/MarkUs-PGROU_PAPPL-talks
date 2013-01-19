ebe7fd5fe93fb639ba342d51f62d72e85b98b977
Benjamin Vialle <benjamin@vialle.io>
2013-01-19 09:17:13 +0100

    Fix sed command and added tips

--- landslide.sh
+++ landslide.sh
@@ -2,3 +2,5 @@
 # Ensure this packages are installed
 # python-jinja2 python-pygments python-markdown
 ./landslide/src/landslide/main.py -r presentation
+
+sed -i 's/\/usr\/local\/lib\/python2.7\/dist-packages\/landslide-1.0.1-py2.7.egg/landslide\/src/g' presentation.html
